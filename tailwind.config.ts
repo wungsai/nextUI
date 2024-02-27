8dfa5d35a75f401ed63f9241ca353beeaaef3ccc
wungsai <33681920+wungsai@users.noreply.github.com>
2024-02-27 11:18:06 +0700

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,18 @@
 import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/react";
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
